80c1692b7d98a76f1de6874d4ff57afaa395db7b
Jack-lss <1349916452@qq.com>
2021-10-16 15:50:17 +0800

    + 15点50分 2021年10月16日

---
+++ 1606.找到处理最多请求的服务器.cpp
@@ -0,0 +1,58 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-15 19:58:38
+ * @LastEditTime: 2021-10-15 20:04:49
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\1606.找到处理最多请求的服务器.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=1606 lang=cpp
+ *
+ * [1606] 找到处理最多请求的服务器
+ */
+
+// @lc code=start
+typedef pair<int,int> pii;
+class Solution {
+public:
+    vector<int> busiestServers(int k, vector<int>& arrival, vector<int>& load) {
+        int n = arrival.size();
+        int target = 0;
+        int maxService = 0;
+        vector<int> ans;
+        priority_queue<pii,vector<pii>,greater<pii>> pq;
+        vector<int> count(k,0);
+        set<int> wait;
+        
+        for(int i = 0; i < k; ++i){
+            wait.insert(i);
+        }        
+        for(int i = 0; i < n; ++i){
+            while(!pq.empty() && pq.top().first <= arrival[i]){
+                wait.insert(pq.top().second);
+                pq.pop();
+            }
+            int curr = i%k;
+            if(wait.empty()) continue;
+            auto it = wait.lower_bound(curr);
+            if(it != wait.end()){
+                target = *it;
+            }else{
+                target = *wait.begin();
+            }
+            count[target]++;
+            wait.erase(target);
+            pq.push(make_pair(arrival[i] + load[i],target));
+            maxService = max(maxService,count[target]);
+        }
+        
+        for(int i = 0; i < k; ++i){
+            if(count[i] == maxService) ans.push_back(i);
+        }
+        
+        return ans;
+    }
+};
+// @lc code=end
+
---
+++ 1914.循环轮转矩阵.cpp
@@ -0,0 +1,65 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-16 15:34:49
+ * @LastEditTime: 2021-10-16 15:46:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \leetcode\1914.循环轮转矩阵.cpp
+ */
+/*
+ * @lc app=leetcode.cn id=1914 lang=cpp
+ *
+ * [1914] 循环轮转矩阵
+ */
+
+// @lc code=start
+class Solution {
+    vector<int> dir = { 0,-1, 1,0, 0,1, -1,0 } ;
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        
+        int rLL=0,rUL = n-1,cLL = 0, cUL = m-1 ;
+        
+        while(rLL<=rUL && cLL <= cUL ){
+            
+            int k2 = k%( (rUL-rLL-1)*2 + 2*(cUL-cLL+1) ) ;
+            
+            for(int i=0;i<k2;i++)
+                move(rLL,rUL,cLL,cUL,grid) ;
+            
+            rLL++;rUL--;
+            cLL++;cUL--;
+        }
+        
+        return grid;
+    }
+
+    void move(int rLL,int rUL,int cLL,int cUL,vector<vector<int>>& grid){
+       
+        int j2 = cUL;
+        int i2 = rLL ;
+        
+        int d = 0 ;
+        int prev=-1;
+        
+        do{
+            swap(grid[i2][j2], prev) ; 
+            
+            int i3 = i2+dir[d],j3 =j2+dir[d+1] ;
+            
+            if( j3 < cLL || i3 < rLL || j3 > cUL || i3 > rUL )
+                d+=2 ;
+                    
+            i2 = i2+dir[d] ;
+            j2 = j2+dir[d+1] ;
+            
+        }while( (i2 != rLL) || (j2 != cUL ) ) ;
+        
+        grid[i2][j2] = prev ;
+        
+    }
+};
+// @lc code=end
+
